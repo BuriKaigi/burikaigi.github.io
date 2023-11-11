757c62b88a0fbf6c25c80345a8a973ba7d50481c
hikaruworld <hikaruworld@gmail.com>
2023-11-11 14:52:13 +0900

    ドメイン変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://burikaigi.github.io',
+  site: 'https://burikaigi.dev',
 });
