c51972c609992f48141c91f75c84b78b77cd0136
hikaruworld <hikaruworld@gmail.com>
2023-12-24 20:27:24 +0900

    スタイル微調整

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -9,7 +9,7 @@ export function Sponsors({ sponsors }: { sponsors: any[] }) {
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-gray-900 sm:text-5xl">
           Sponsors
         </h2>
-        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
+        <div className="mx-auto mt-20 mx-20 grid max-w-max grid-cols-1 place-content-center gap-x-16 gap-y-12 sm:grid-cols-3 md:gap-x-32 lg:gap-x-16">
           {sponsors.length == 0 && (
             <div className="text-center col-span-3">調整中</div>
           )}
