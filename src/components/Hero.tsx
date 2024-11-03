44fc1fc779adfa05cdda5c0ec898163db05786fa
Masayuki Katoh <kato@phalanxware.com>
2024-11-03 09:47:05 +0900

    パス修正

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-5xl font-bold tracking-tighter text-black-600 sm:text-7xl">
-            <a href="/2024/">BuriKaigi 2024</a>
+            <a href="/">BuriKaigi 2024</a>
           </h1>
           <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-graye-900">
             <p>
